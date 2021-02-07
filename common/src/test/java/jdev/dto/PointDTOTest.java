db23ec1397cb2fd269ae4ffb0b223c9565ad5cde
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-02-07 20:33:12 +0500

    Преобразование объекта в строку и обратно

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -1,8 +1,10 @@
 package jdev.dto;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -10,12 +12,24 @@ import static org.junit.Assert.assertTrue;
  */
 public class PointDTOTest {
 
+    private String Expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1612711229676}";
+    private String autoId = "o567gfd";
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
         point.setLat(56);
         point.setLon(74);
         point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
         assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(Expected, PointDTO.class);
+        assertEquals (1612711229676L, dto.getTime());
     }
 }
