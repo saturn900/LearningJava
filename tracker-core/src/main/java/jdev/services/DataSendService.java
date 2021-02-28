fe22571d5b43c31ec30ec72f66fd008c5005408f
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-02-28 20:51:57 +0500

    Блокирующая очередь не работает

--- tracker-core/src/main/java/jdev/services/DataSendService.java
+++ tracker-core/src/main/java/jdev/services/DataSendService.java
@@ -1,13 +1,35 @@
 package jdev.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jdev on 26.03.2017.
- */
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+
 @Service
 public class DataSendService {
-    public void callFromInit() {
-        System.out.println("DataSendService.callFromInit");
+    private static final Logger log = LoggerFactory.getLogger(DataPeekService.class);
+
+    private BlockingDeque<String> queue = new LinkedBlockingDeque<>(100);
+    private int putCount;
+    private long previous;
+
+    // public void callFromInit() {
+    //   System.out.println("шляпа");
+    //}
+
+    @Scheduled(fixedDelay = 1_000)
+    void take() throws InterruptedException {
+        int i = putCount++;
+        log.info("take trying!!!");
+        long current = System.currentTimeMillis();
+        //log.info((current - previous) + " ScheduledQueueService.take " + queue.poll(500, TimeUnit.MILLISECONDS));
+        System.out.println((current - previous) + " ScheduledQueueService.take " + queue.take());
+        previous = current;
+
     }
+
 }
