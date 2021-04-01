846788140540da18b666f929e89b5a61c246061f
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-04-01 16:23:44 +0500

    8. server-core создание интерфейса GPSCoordRepository

---
+++ server-core/src/main/java/jdev/dao/repo/GPSCoordRepository.java
@@ -0,0 +1,7 @@
+package jdev.dao.repo;
+
+import jdev.dao.GPSCoord;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GPSCoordRepository extends CrudRepository<GPSCoord, Integer> {
+}
