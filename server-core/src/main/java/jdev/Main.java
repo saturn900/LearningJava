109ea891073e50f7cbc20055ebc01188ab924b9b
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-04-03 15:52:11 +0500

    8. GPSCoord не запускается Main

--- server-core/src/main/java/jdev/Main.java
+++ server-core/src/main/java/jdev/Main.java
@@ -2,15 +2,21 @@ package jdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 /**
  * Created by Егор on 25.11.2017.
  */
+
 @SpringBootApplication
-@ComponentScan({"jdev.services"}) // неверные имена пакетов в которых уже спринг будет искать бины!!! были, теперь норм, запускабельно
+@EnableJpaRepositories("jdev.dao")
+@EntityScan(basePackageClasses = jdev.dao.GPSCoord.class)
+@ComponentScan({"jdev.services","jdev.dao"})
 public class Main{
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
