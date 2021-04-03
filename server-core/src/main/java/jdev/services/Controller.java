d8f67aee858f7966f25a693d0d004b3802bf907c
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-04-03 11:19:01 +0500

    8. server-core попытка 1 сконфигурировать Controller

--- server-core/src/main/java/jdev/services/Controller.java
+++ server-core/src/main/java/jdev/services/Controller.java
@@ -1,16 +1,31 @@
 package jdev.services;
 
+import jdev.dao.GPSCoord;
+import jdev.dao.repo.GPSCoordRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Егор on 25.11.2017.
  */
+@EnableJpaRepositories("jdev.dao")
+@EntityScan(basePackageClasses = jdev.dao.GPSCoord.class)
+@SpringBootApplication
 @RestController
-public class Controller {
+public class Controller implements CommandLineRunner {
     Logger logger = LoggerFactory.getLogger(Controller.class);
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+    private List<GPSCoord> all;
     private long previous;
 
     @Autowired
@@ -30,6 +45,43 @@ public class Controller {
         return DataGps;
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
-    //можно добавить метод с аннотацией PostConstruct и в нем вывод в лог, чтобы проверить что данный контроллер инициализируется спрингом
+    @Autowired
+    GPSCoordRepository gpsCoordRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        read();
+    }
+
+    private void delete(GPSCoord gpsCoord) {
+        gpsCoordRepository.delete(gpsCoord);
+    }
+
+    private void update(GPSCoord gpsCoord, String model) {
+        gpsCoord.setModel(model);
+        gpsCoordRepository.save(gpsCoord);
+    }
+
+    private void read() {
+        all = (List<GPSCoord>) gpsCoordRepository.findAll();
+
+        if (all.size() == 0) {
+            log.info("NO RECORDS");
+        } else {
+            all.stream().forEach(rocket -> log.info(rocket.toString()));
+        }
+    }
+
+    private GPSCoord create(String model) {
+        GPSCoord gpsCoord = new GPSCoord();
+        gpsCoord.setModel(model);
+        return gpsCoordRepository.save(gpsCoord);
+    }
 }
+
+
