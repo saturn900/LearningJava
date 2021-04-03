5977fe3983f269fbe8fe549fa9e520055351df14
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-04-03 19:43:29 +0500

    8. Service-core откат

--- server-core/src/main/java/jdev/Main.java
+++ server-core/src/main/java/jdev/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by Егор on 25.11.2017.
  */
 @SpringBootApplication
-@ComponentScan({"jdev.services"}) // неверные имена пакетов в которых уже спринг будет искать бины!!! были, теперь норм, запускабельно
+@ComponentScan({"jdev.services"}) //  Начальная точка
 public class Main{
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
