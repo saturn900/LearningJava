a7c81f60e53449fea02a2a1d8c338877d0d872bc
rusk-sonOfACucumber <78471760+rusk-sonOfACucumber@users.noreply.github.com>
2021-04-03 19:37:24 +0500

    8. Service-core не работает

--- server-core/src/main/java/jdev/Main.java
+++ server-core/src/main/java/jdev/Main.java
@@ -1,16 +1,16 @@
 package jdev;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Егор on 25.11.2017.
  */
-
-
-@ComponentScan({"jdev.services","jdev.repo"})
+@SpringBootApplication
+@ComponentScan({"jdev.services"}) // неверные имена пакетов в которых уже спринг будет искать бины!!! были, теперь норм, запускабельно
 public class Main{
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
--- server-core/src/main/java/jdev/services/Controller.java
+++ server-core/src/main/java/jdev/services/Controller.java
@@ -1,23 +1,13 @@
 package jdev.services;
 
-import jdev.repo.GPSCoordRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Егор on 25.11.2017.
  */
-
-@SpringBootApplication
-@EnableJpaRepositories("jdev.repo")
-@EntityScan(basePackageClasses = jdev.services.GPSCoord.class)
 @RestController
 public class Controller {
     Logger logger = LoggerFactory.getLogger(Controller.class);
@@ -26,9 +16,8 @@ public class Controller {
     @Autowired
     FileService FileService;
 
-
-    @Autowired
-    GPSCoordRepository gpsCoordRepository;
+//    @Autowired
+//    DataSendService dataSendService;
 
 
     @RequestMapping(value = "DataGps",method = RequestMethod.POST)
@@ -40,12 +29,6 @@ public class Controller {
 //        FileService.writeInFile((current - previous) +"AutoId" + DataGps.getAutoId() + " lon = " + DataGps.getLon() + " lat = " + DataGps.getLat());
         return DataGps;
     }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
 
 
     //можно добавить метод с аннотацией PostConstruct и в нем вывод в лог, чтобы проверить что данный контроллер инициализируется спрингом
